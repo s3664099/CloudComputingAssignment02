227abc75e6d0f6d1c1eac29d3c6c08b531c851de
s3664099 <s3664099@student.rmit.edu.au>
2020-04-16 00:35:53 +1000

    dynamic website with python and DB working

--- main.py
+++ main.py
@@ -20,13 +20,14 @@ For more information, see the README.md.
 """
 
 # [START gae_python_mysql_app]
-import os
-#import urllib
 
+#Imports
+import os
 import MySQLdb
 import webapp2
 import jinja2
 
+#This function sets up the jinj enviroment
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
@@ -38,11 +39,13 @@ CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
 CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
 CLOUDSQL_DB = os.environ.get('CLOUDSQL_DB')
 
+#Establish a class to hold variables pulled from the static HTML page
 class search ():
     locale_type = "All"
     locale_place = "Sunbury"
 
 
+#Function to connect to the SQL database in google cloud
 def connect_to_cloudsql():
     # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
     # will be set to 'Google App Engine/version'.
@@ -52,6 +55,7 @@ def connect_to_cloudsql():
         cloudsql_unix_socket = os.path.join(
             '/cloudsql', CLOUDSQL_CONNECTION_NAME)
 
+        #This function here is what you use to connect to the SQL database
         db = MySQLdb.connect(
             unix_socket=cloudsql_unix_socket,
             user=CLOUDSQL_USER,
@@ -71,36 +75,54 @@ def connect_to_cloudsql():
 
     return db
 
-
+#This class is the main page handler. The code in this class processes what gets placed
+#Onto the HTML file.
 class MainPage(webapp2.RequestHandler):
+
+    #The get function, while it sounds odd, the get is actually being called
+    #from the HTML file as opposed to this file.
     def get(self):
         """Simple request handler that shows all of the MySQL variables."""
 
+        #Sets an empty dictionary to handle the template values
+        #These are the values that wull be used by the HTML file
         template_values = {}
 
         if search.locale_type != "":
             template_values = MainPage.perform_search(self)
 
+        #This code sends the template values to the HTML file.
+        #The first line sets up the template values, the second line renders the webpage.
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
+#    def database_manip(self, db):
+
+    #This function is where all of the database searches are performed
     def perform_search(self):
 
+        #Set up the values to be used in this function
         locale_type = search.locale_type
         locale_place = search.locale_place
+        locations = []
+        location_details = {}
+        template_values = {}
+        towns = []
 
         number = 0
         db = connect_to_cloudsql()
+        #MainPage.database_manip(self, db)
+
+        #Sets up the function that performs the searches and stores the results
+        #The queries are below.
         cursor = db.cursor()
         if locale_type == 'All':
             cursor.execute("SELECT localename, address, telephone, localtype FROM place WHERE town = '"+locale_place+"' and state = 'Vic' LIMIT 20")
         else:
             cursor.execute("SELECT localename, address, telephone, localtype FROM place WHERE localtype ='"+locale_type+"' and town = '"+locale_place+"' and state = 'Vic' LIMIT 20")
 
-        locations = []
-        location_details = {}
-        template_values = {}
-
+        #Now that we have performed the queries, the results are processed and stored
+        #in the dictionary which is then passed back to the main function
         for locale, address, telephone, localtype in cursor.fetchall():
             number +=1
 
@@ -131,19 +153,36 @@ class MainPage(webapp2.RequestHandler):
             for icon, localtype in cursor.fetchall():
                 location.update({"localtype1": icon})
 
+        cursor.execute("SELECT town, state FROM location WHERE state = 'Vic'")
+
+        for town, state in cursor.fetchall():
+            town_details = {
+                "town": town,
+            }
+            towns.append(town_details)
+
+        #The results are stored in the template values for use on the webpage
         template_values = {
            'location_details': locations,
+           'towns': towns
         }           
         return template_values
 
+#This is the handler for recieving data from the webpage.
 class Search(webapp2.RequestHandler):
     def post(self):
 
+        #These functions are where the data is recieved and stored
+        #in the class to hold the data. The tags inside the get correspond
+        #with the name in the form on the html page
         search.locale_type = self.request.get('type')
         search.locale_place = self.request.get('suburb')
+
+        #The program is then sent to the MainPage to update the page
         self.redirect('/')
 
 
+#This function defines where data is to go. 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/search', Search)
